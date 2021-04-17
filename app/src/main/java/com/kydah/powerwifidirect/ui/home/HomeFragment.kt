b8060d4c07a089ce8db86235ed0d0c4778198b97
mug1wara26 <aloysiusgsq@gmail.com>
2021-04-17 20:46:37 +0800

    Justin can u add the proper download button

--- app/src/main/java/com/kydah/powerwifidirect/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kydah/powerwifidirect/ui/home/HomeFragment.kt
@@ -19,13 +19,10 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        
+
         return root
     }
 }
